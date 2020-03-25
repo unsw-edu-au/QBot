6e7f70485ee77c5466e568b6ec6cd644213da8a9
vishnuabhrapudi <58915530+vishnuabhrapudi@users.noreply.github.com>
2020-03-25 16:11:03 +0530

    solution filepath changes

--- deploy.bot.cmd
+++ deploy.bot.cmd
@@ -67,7 +67,7 @@ SET MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 echo Handling ASP.NET Core Web Application deployment.
 
 :: 1. Restore nuget packages
-call :ExecuteCmd dotnet restore "%DEPLOYMENT_SOURCE%\Source\Microsoft.Teams.Apps.QBot.Bot.sln"
+call :ExecuteCmd dotnet restore "%DEPLOYMENT_SOURCE%\Source\Microsoft.Teams.Apps.QBot.sln"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. Build and publish
