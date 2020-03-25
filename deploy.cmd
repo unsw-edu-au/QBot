d380b30750bc015788bb7370c8ac76e7b5c3e04e
vishnuabhrapudi <58915530+vishnuabhrapudi@users.noreply.github.com>
2020-03-25 16:02:38 +0530

    Bot service only to test

--- deploy.cmd
+++ deploy.cmd
@@ -3,18 +3,7 @@
 IF "%SITE_ROLE%" == "bot" (
   deploy.bot.cmd
 ) ELSE (
-	IF "%SITE_ROLE%" == "dashboard" (
-		deploy.dashboard.cmd
-	) ELSE (
-		IF "%SITE_ROLE%" == "questions" (
-			deploy.questions.cmd
-		) ELSE (
-			IF "%SITE_ROLE%" == "function" (
-				deploy.function.cmd
-			) ELSE (
-				echo You have to set SITE_ROLE setting to either "bot" or "configuration"
-				exit /b 1
-			)
-		)
+			echo You have to set SITE_ROLE setting to either "bot" or "dashboard" or "questions" or "function"
+			exit /b 1
 	)
 )
