7aa4b49bb63b95f15024a6bbcabc67847ca4748e
vishnuabhrapudi <58915530+vishnuabhrapudi@users.noreply.github.com>
2020-04-01 18:04:11 +0530

    Update deploy.dashboard.cmd

--- deploy.dashboard.cmd
+++ deploy.dashboard.cmd
@@ -81,7 +81,7 @@ echo Temp %DEPLOYMENT_TEMP%
 echo Target %DEPLOYMENT_TARGET%
 echo manifest path %NEXT_MANIFEST_PATH% 
 
-call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\Source\DashboardTabApp\wwwroot" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
