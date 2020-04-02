45e1cfefe72f5dac9e4aea5701d2b7a2f88f0fe1
vishnuabhrapudi <58915530+vishnuabhrapudi@users.noreply.github.com>
2020-04-02 17:57:48 +0530

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbottesting14.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbottesting15.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
